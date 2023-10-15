13e6ebb456882eae969573e229db82d40af20a4a
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-10-15 14:42:50 -0500

    route tested with postman info appears correct

--- server/routes/handicap.router.js
+++ server/routes/handicap.router.js
@@ -2,13 +2,50 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool");
 
-router.get("/users/:id", async (req, res) => {
-  const userId = req.params.id;
+router.get("/calculate/:userId", async (req, res) => {
+  const userId = req.params.userId;
   try {
-    const result = await pool.query("SELECT * FROM users WHERE id = $1", [
-      userId,
-    ]);
-    res.json(result.rows[0]);
+    // Fetch last 20 rounds of golf scores, related course data, and user gender from the database
+    const result = await pool.query(
+      `
+      SELECT 
+        ur.front_9_score, ur.back_9_score, 
+        uc.men_course_rating, uc.men_course_slope, 
+        uc.women_course_rating, uc.women_course_slope,
+        u.is_male
+      FROM user_rounds ur 
+      JOIN user_courses uc ON ur.course_id = uc.id
+      JOIN users u ON ur.user_id = u.id
+      WHERE ur.user_id = $1 
+      ORDER BY ur.date DESC 
+      LIMIT 20`,
+      [userId]
+    );
+
+    // Calculate score differentials considering gender
+    const scoreDifferentials = result.rows.map((row) => {
+      const totalScore = row.front_9_score + row.back_9_score;
+      const courseRating = row.is_male
+        ? row.men_course_rating
+        : row.women_course_rating;
+      const courseSlope = row.is_male
+        ? row.men_course_slope
+        : row.women_course_slope;
+      const scoreDifferential =
+        ((totalScore - courseRating) * 113) / courseSlope;
+      return scoreDifferential;
+    });
+
+    // Sort score differentials, select the lowest 8, and calculate average
+    const bestDifferentials = scoreDifferentials
+      .sort((a, b) => a - b)
+      .slice(0, 8);
+    const avgDifferential =
+      bestDifferentials.reduce((a, b) => a + b, 0) / bestDifferentials.length;
+
+    // Calculate and send the handicap index
+    const handicapIndex = avgDifferential * 0.96;
+    res.json({ handicap: handicapIndex.toFixed(1) });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [handicap, setHandicap] = useState(null);
+
+  useEffect(() => {
+    const userId = 1;
+
+    axios
+      .get(`/api/handicap/calculate/${userId}`)
+
+      .then((response) => {
+        setHandicap(response.data.handicap);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Golf Spike</h1>
+        {handicap ? <p>User's Handicap: {handicap}</p> : <p>Loading...</p>}
       </header>
     </div>
   );
